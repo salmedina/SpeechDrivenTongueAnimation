c2b146805142f9c902a125f0995556832ff5f0e3
salmedina <salbed.medina@gmail.com>
2022-03-24 04:18:51 -0400

    Add calc_seq_dist function

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ import torch
 import soundfile as sf
 
 
-def seed_everything(seed: int):
+def seed_everything(seed: int) -> None:
     """ Seed all random number generators
 
     Args:
@@ -22,7 +22,7 @@ def seed_everything(seed: int):
     torch.cuda.manual_seed_all(seed)
 
 
-def init_torch_device(gpu_id=-1):
+def init_torch_device(gpu_id: int=-1) -> torch.device:
     """ Creates torch device based on the GPU ID
 
     Args:
@@ -37,7 +37,7 @@ def init_torch_device(gpu_id=-1):
     return torch.device(device_str)
 
 
-def load_audio(path):
+def load_audio(path: str) -> tuple:
     """ Load and normalize audio file
 
     Args:
@@ -55,3 +55,32 @@ def load_audio(path):
         else:
             audio_signal = audio_signal.mean(axis=1)  # mean value across all channels
     return audio_signal, sample_rate
+
+
+def calc_seq_dist(gt_tensor: torch.Tensor,
+                  pred_tensor: torch.Tensor) -> float:
+    """ Calculates the sequence MSE from tensors
+
+    Args:
+        gt_tensor (torch.Tensor): ground truth
+        pred_tensor (torch.Tensor): predictions values
+
+    Returns:
+        float: MSE
+    """
+    # Reshape the tensors to 3D coords
+    gt_tensor = gt_tensor.reshape(-1, 10, 3)
+    pred_tensor = pred_tensor.reshape(-1, 10, 3)
+
+    # Calculate batch L2 error
+    # -- Axis; 0: frames, 1:joints, 2:coords
+    pose_delta = gt_tensor - pred_tensor
+    error_tensor = torch.sqrt(torch.sum(
+                              torch.mul(pose_delta, pose_delta),
+                              axis=2))
+    # mean across the joints through the sequence,
+    # then the mean across the joints
+    joints_error = error_tensor.mean(axis=0)
+    sample_error = joints_error.mean().cpu().item()
+
+    return sample_error
